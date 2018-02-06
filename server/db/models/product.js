cb941fd473e81573cad99311cc5b0f7ad3deb63d
max-allen <maxton.allen@gmail.com>
2018-02-05 21:45:01 -0500

    product model

---
+++ server/db/models/product.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	category: {
+		type: Sequelize.STRING
+		allowNull: false
+
+	},
+	description: {
+		type: Sequelize.STRING
+		allowNull: false
+	},
+	price: {
+		type: Sequelize.INTEGER,
+		validate: {
+			min: 0
+		}
+	},
+	inventory: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+		defaultValue: 1
+		validate: {
+			min: 0
+		}
+	},
+	rating: {
+		type: Sequelize.ENUM(1,2,3,4,5),
+		defaultValue: null,
+	},
+	photo: {
+		type: Sequelize.STRING,
+		validate: {
+			isUrl: true
+		}
+	}
+})
+
+module.exports = Product
